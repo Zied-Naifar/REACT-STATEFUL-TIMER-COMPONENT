01a60603af8b03c92f19b3a4216ade3a8ad47553
Zied-Naifar <Zied-Naifar@github.com>
2019-02-12 15:24:29 +0100

    Checked Version

--- src/Timer.js
+++ src/Timer.js
@@ -1,62 +1,61 @@
 import React from 'react'
 
-var seconds;
-var hours;
-var minutes;
-        
-function msToHMS( ms ) {
-    //console.log(ms)
-    hours = Math.floor(ms / 3600000);
-    minutes = Math.floor((ms - (hours * 3600000)) / 60000);
-    seconds = parseInt((ms - (hours * 3600000) - (minutes * 60000)) / 1000);
-    console.log(hours, minutes, seconds)
-}
-
 class Timer extends React.Component{
 
     constructor(props){
         super(props)
         this.state = {
             status: false,
-            runningTime: 0
+            runningTime: 0,
+            seconds:0,
+            hours:0,
+            minutes:0
         }
-    }
+      
+        setInterval(
+            () => {
+            if(this.state.status){ 
+            this.setState({
+                runningTime: this.state.runningTime + 1000
+            })
+            this.msToHMS(this.state.runningTime)
+        } 
+        },
+            1000
+            )
+        }
     
+    
+    msToHMS = ( ms ) => {
+        this.setState({
+        hours: Math.floor(ms / 3600000),
+        minutes: Math.floor((ms - (this.state.hours * 3600000)) / 60000),
+        seconds: parseInt((ms - (this.state.hours * 3600000) - (this.state.minutes * 60000)) / 1000)
+        })
+    }
+
     Click = () => {
-        this.setState(state => {
-            if (state.status) {
-                clearInterval(this.timer);
-            } else {
-                const startTime = Date.now() - this.state.runningTime;
-                this.timer = setInterval(() => {
-                this.setState({ runningTime: Date.now() - startTime },()=>msToHMS(this.state.runningTime));
-                
-                });
-            }
-            return { status: !state.status };
-            }
-        );
-    };
+        this.setState( {status: !this.state.status} )
+    }
   
     Reset = () => {
-        this.setState({ runningTime: 0, status: false },()=>msToHMS(this.state.runningTime));
+        this.setState({ status:false, runningTime: 0, seconds:0, hours:0, minutes:0 });
     };
     
-    
-    
+
     render() {
     return (<div className="container">
                 <div className="full-timer-container">
                     <div className="time-container">
-                        <p className="timer-form">{hours}:</p>
+                        <p className="timer-form">{String(this.state.hours).padStart(2, '0')}:</p>
                         <p className="timer-comment">hours</p>
                     </div>
                     <div className="time-container">
-                        <p className="timer-form">{minutes}:</p>
+                        <p className="timer-form">{String(this.state.minutes).padStart(2, '0')}:</p>
                         <p className="timer-comment">Minute</p>
                     </div>
                     <div className="time-container">
-                        <p className="timer-form">{seconds}</p>
+                        <p className="timer-form">{String(this.state.seconds).padStart(2, '0')}</p>
                         <p className="timer-comment">second</p>
                     </div>
                 </div>
